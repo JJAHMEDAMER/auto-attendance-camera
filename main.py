721c46107d6348e923ac7237ffa7e3160fd3d520
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-10 01:04:00 +0200

    convert to base64 & send images

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
 import base64
 
-with open("1.jpg", "rb") as img_file:
-    my_string = base64.b64encode(img_file.read())
 
 res = requests.get("http://127.0.0.1:8000/cam")
+print(res.json())
 
 
+with open("man.jpeg", "rb") as img_file:
+    my_string = base64.b64encode(img_file.read())
+    # print((my_string).decode('ascii'))
+    
 payload = {
-    "image": str(my_string)
+    "image": my_string.decode('ascii')
 }
 
 res = requests.post("http://127.0.0.1:8000/cam", json=payload)
-
 print(res.json())
