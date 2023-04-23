2a7afd5784b3c31efdbe7dac174c25370fe7eaa4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-04-23 16:07:17 +0200

    add location of course  in the payload

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ res = requests.get("http://127.0.0.1:8000/cam")
 print(res.json())
 
 
-with open("man.jpeg", "rb") as img_file:
+with open("test_images/individual.jpg", "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
     # print((my_string).decode('ascii'))
     
 payload = {
+    "location": "Hall B",
     "image": my_string.decode('ascii')
 }
 
